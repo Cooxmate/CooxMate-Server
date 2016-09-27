c85aae90a9dea842cb594874eec2723d35c37264
PanajotisMaroungas <panajotis.maroungas@gmail.com>
2016-09-27 21:18:56 +0200

    init project

---
+++ Cooxmate/src/com/cooxmate/server/CooxmateServlet.java
@@ -0,0 +1,12 @@
+package com.cooxmate.server;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class CooxmateServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
